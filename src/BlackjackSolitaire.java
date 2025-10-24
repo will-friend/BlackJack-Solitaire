eeb09d6b9007bf260d58105c7de626f3f886e047
will-friend <wsfriend3@gmail.com>
2025-10-24 18:47:10 -0400

    First initial game implementation, and updated Board class to take in player entry and calculation the row and column correctly rather than ahving game do it.

--- src/BlackjackSolitaire.java
+++ src/BlackjackSolitaire.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BlackjackSolitaire {
 
     private Deck deck;
     private Board board;
+    static Scanner sc = new Scanner(System.in);
 
     public BlackjackSolitaire() {
         this.deck = new Deck();
@@ -37,7 +39,38 @@ public class BlackjackSolitaire {
                 handScore -= 10;
             }
             gameSum += scoreHand(handScore, false);
+            aceCount = 0;
         }
+
+        for (int j = 0; j < gameBoard[0].length; j++) {
+            int handScore = 0;
+            for (int i = 0; i < gameBoard.length; i++) {
+                if (gameBoard[i][j] == null) {
+                    continue;
+                } else if (((i == gameBoard.length - 1 || i == gameBoard.length - 2)) && ((j == gameBoard[0].length - 1) || (j == 0))) {
+                    continue;
+                } else if (gameBoard[i][j].getValue() == 1) {
+                    if (aceCount == 0 && handScore <= 10) {
+                        aceCount++;
+                        handScore += 11;
+                    } else if (aceCount >= 1) {
+                        handScore += 1;
+                    } else {
+                        handScore += gameBoard[i][j].getValue();
+                    }
+                }
+            }
+            if (handScore > 21 && aceCount >= 1) {
+                handScore -= 10;
+            }
+            if (j == 0 || j == 4) {
+                gameSum += scoreHand(handScore, true);
+            } else {
+                gameSum += scoreHand(handScore, false);
+            }
+            aceCount = 0;
+        }
+
         return gameSum;
     }
 
@@ -80,8 +113,41 @@ public class BlackjackSolitaire {
                 "a new action.\n" +
                 "The goal of the game is to place (or discard) each new card presented, trying to get a sum of 21 on any row or " +
                 "column of the game board. The game ends only when the board is completely filled. A player gets a total of four" +
-                "discards per game (represented by the 2x2 discard grid)."
+                "discards per game (represented by the 2x2 discard grid). Let's start\n\n\n"
         );
+        int disCards = 4;
+        while (!this.isBoardFull()) {
+            Card topCard = deck.getTopCard();
+            System.out.println("Drawn Card: " + topCard.toString());
+            System.out.println("Game Board:");
+            board.printBoard();
+            System.out.println("Discard Board:");
+            board.printDiscard();
+
+            boolean tilePlaced = false;
+
+            do {
+                System.out.println("Please select the tile you would like to play the card on (1-16 on the game board, 17-20 for discard): ");
+                int tile = sc.nextInt();
+                if (tile >= 1 && tile <= 16) {
+                    tilePlaced = board.placeCard(tile, topCard);
+                } else if (tile >= 17 && tile <= 20) {
+                    tilePlaced = board.discardCard(tile, topCard);
+                    if (tilePlaced) {
+                        disCards--;
+                        System.out.println("Discards remaining: " + disCards);
+                    }
+                } else {
+                    System.out.println("Invalid tile number, please reference board and try again.");
+                }
+
+            } while (!tilePlaced);
+
+        }
+
+        int gameScore = sumScore();
+
+        System.out.println("Game has finished!\nGame Score: " + gameScore);
 
 
 
--- src/Board.java
+++ src/Board.java
@@ -30,6 +30,43 @@ public class Board {
         return board;
     }
 
+    private int getColumn(int tileNum, int row) {
+
+        int col = 0;
+        if (row < this.board.length / 2) {
+            col = ((tileNum-1) % this.board[0].length);
+        } else if (row == this.board.length / 2) {
+            if (tileNum == 17) {
+                col = (10 % this.board[0].length);
+            } else if (tileNum == 18) {
+                col = (14 % this.board[0].length);
+            } else {
+                col = (tileNum) % this.board[0].length;
+            }
+        } else if (row == this.board.length - 1) {
+            if (tileNum == 19) {
+                col = (15 % this.board[0].length);
+            } else if (tileNum == 20) {
+                col = (19 % this.board[0].length);
+            } else {
+                col = (tileNum+2) % this.board[0].length;
+            }
+        }
+
+        return col;
+
+    }
+
+    private int getRow(int tileNum) {
+        int row = 0;
+        if (tileNum == 17 || tileNum == 18) {
+            row = (tileNum - board[0].length) / board[0].length;
+        } else {
+            row = (tileNum - 1) / this.board[0].length;
+        }
+        return row;
+    }
+
     /**
      * Method allowing player to place a card on empty space on regular game board
      * @param row row index of board
@@ -37,7 +74,11 @@ public class Board {
      * @param card card object to be placed onto the board
      * @return boolean flag indicating whether card was placed or not
      */
-    public boolean placeCard(int row, int col, Card card) {
+    public boolean placeCard(int tileNum, Card card) {
+
+        int row = getRow(tileNum);
+        int col = getColumn(tileNum, row);
+
         if ((row == 2 || row == 3) && (col == 0 || col == 4)) {
             System.out.println("Cannot place tile in discard slot, please enter a valid slot.");
             return false;
@@ -56,7 +97,10 @@ public class Board {
      * @param card card object to be placed onto the board
      * @return boolean flag indicating whether card was discarded or not
      */
-    public boolean discardCard(int row, int col, Card card) {
+    public boolean discardCard(int tileNum, Card card) {
+
+        int row = getRow(tileNum);
+        int col = getColumn(tileNum, row);
 
         if (board[2][0] != null && board[2][4] != null && board[3][0] != null && board[3][4] != null) {
             System.out.println("No more slots to discard to, please play the card.");
@@ -67,7 +111,7 @@ public class Board {
             this.board[row][col] = card;
             return true;
         }
-        System.out.println("Cannot discard a card to a playable slot, please enter a valid discard slot.");
+        System.out.println("Invalid entry, please enter a valid discard slot.");
         return false;
     }
 
@@ -128,8 +172,8 @@ public class Board {
         Card c1 = new Card("Heart", 3);
         Card c2 = new Card("Diamond", 2);
 
-        board.placeCard(0,0, c1);
-        board.discardCard(2, 0, c2);
+        board.placeCard(1, c1);
+        board.discardCard(18, c2);
         System.out.println("Board:");
         board.printBoard();
         System.out.println("Discard:");
